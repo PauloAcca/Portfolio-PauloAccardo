fbb39da3c753cc451f1592d25ef70f2dbd67106e
Paulo Accardo <88637414+PauloAcca@users.noreply.github.com>
2023-08-07 19:55:48 -0300

    Update aboutMe.jsx

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -21,7 +21,7 @@ export const AboutMe = ()=>{
                 <div className='containerImg'>
                     <div className="card">
                         <div className="card-info">
-                            <img className='img-fluid' src="public/img/Imagen-Perfil.jpg" alt=""/>
+                            <img className='img-fluid' src="https://github.com/PauloAcca/Portfolio-PauloAccardo/blob/main/public/img/Imagen-Perfil.jpg" alt=""/>
                         </div>
                     </div>
                 </div>
