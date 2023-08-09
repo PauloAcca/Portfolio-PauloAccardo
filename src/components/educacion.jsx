adfe5096d3fc48e0645f1bc25b10628107e626b8
PauloAcc <paulo.matias.accardo@gmail.com>
2023-08-09 19:58:00 -0300

    puruebo ruta imagen

--- src/components/educacion.jsx
+++ src/components/educacion.jsx
@@ -6,21 +6,21 @@ export const Educacion = ()=>{
             <div className='barraLateral'></div>
             <div className='containerEstudios text-warning'>
                 <div className='containerEstudio'> 
-                    <p className='tituloEstudio text-light blockquote'>Lic. Ciencia de la Computacion (UBA) </p>
-                    <p className='anoEstudio blockquote-footer'>2020 - Actualidad</p>
+                    <p className='tituloEstudio text-light blockquote'>Lic. Computer Science (UBA) </p>
+                    <p className='anoEstudio blockquote-footer'>2020 - currently</p>
                     <p className='descripcionEstudio text-muted'></p>
                     <link rel="stylesheet" href="" />
                     <div className='barrita'></div>
                 </div> 
                 <div className='containerEstudio'> 
-                    <p className='tituloEstudio text-light blockquote'>Desarrollador Web Full Stack (Digital House)</p>
-                    <p className='anoEstudio blockquote-footer'>Diciembre 2022 - Junio 2023</p>
-                    <p className='descripcionEstudio'>Curso intensivo en que aprendimos a utilizar diferentes herramientas tanto de Front End como Back End</p>
+                    <p className='tituloEstudio text-light blockquote'>Web Developer Full Stack (Digital House)</p>
+                    <p className='anoEstudio blockquote-footer'>December 2022 - June 2023</p>
+                    <p className='descripcionEstudio'>Intensive course in which we learned to use different tools, both Front End and Back End</p>
                     <link rel="stylesheet" href="" />
                     <div className='barrita'></div>
                 </div> 
                 <div className='containerEstudio'> 
-                    <p className='tituloEstudio text-light blockquote'>Escuela secundaria (Cardenal Copello)</p>
+                    <p className='tituloEstudio text-light blockquote'>high school (Cardenal Copello)</p>
                     <p className='anoEstudio'>2014 - 2019</p>
                     <p className='descripcionEstudio text-muted'></p>
                     <link rel="stylesheet" href="" />
